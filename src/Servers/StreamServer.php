d98ed3f0d8d8c47ce510c3dd6881ca6471e2fcc1
wong1989 <skyandsunning@163.com>
2018-04-20 16:05:55 +0800

    version

--- src/Servers/StreamServer.php
+++ src/Servers/StreamServer.php
@@ -30,4 +30,16 @@ abstract class StreamServer
     public function init(){
         return false;
     }
+
+    public function __get($name)
+    {
+        // TODO: Implement __get() method.
+        return $this->{$name};
+    }
+
+    public function __set($name, $value)
+    {
+        // TODO: Implement __set() method.
+        $this->{$name} = $value;
+    }
 }
